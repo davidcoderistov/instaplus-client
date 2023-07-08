67db0be4a08b704befd9bde06e06545c0ee5eca5
davidcoderistov <davidcoderistov@gmail.com>
2023-07-08 16:20:52 +0200

    Configure context

---
+++ src/config/context.ts
@@ -0,0 +1,14 @@
+import { createContext } from 'react'
+import { User } from '../types'
+
+
+export interface AppContext {
+    loggedInUser: User | null
+    setLoggedInUser: (user: User | null) => void
+}
+
+export default createContext<AppContext>({
+    loggedInUser: null,
+    setLoggedInUser: () => {
+    },
+})
