95c45d9759e66403dc11ece57e5ccb32ce5e4c90
davidcoderistov <davidcoderistov@gmail.com>
2023-07-08 16:21:56 +0200

    Write auth graphql mutations

---
+++ src/graphql/mutations/auth/index.ts
@@ -0,0 +1,53 @@
+import { gql } from '@apollo/client'
+
+
+export const SIGN_UP = gql`
+    mutation signUp($firstName: String!, $lastName: String!, $password: String!, $username: String!) {
+        signUp(firstName: $firstName, lastName: $lastName, password: $password, username: $username) {
+            user {
+                _id
+            }
+        }
+    }
+`
+
+export const SIGN_IN = gql`
+    mutation signIn($username: String!, $password: String!) {
+        signIn(username: $username, password: $password) {
+            user {
+                _id
+                firstName
+                lastName
+                username
+                photoUrl
+            }
+            accessToken
+        }
+    }
+`
+
+export const REFRESH = gql`
+    mutation refresh {
+        refresh {
+            user {
+                _id
+                firstName
+                lastName
+                username
+                photoUrl
+            }
+            accessToken
+        }
+    }
+`
+
+export const LOGOUT = gql`
+    mutation logout {
+        logout {
+            user {
+                _id
+                username
+            }
+        }
+    }
+`
