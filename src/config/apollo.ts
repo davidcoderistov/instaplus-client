e196af67d2acb617a041afd3c7290a83a572a7c5
davidcoderistov <davidcoderistov@gmail.com>
2023-07-08 16:20:30 +0200

    Configure apollo

---
+++ src/config/apollo.ts
@@ -0,0 +1,66 @@
+import { ApolloClient, ApolloLink, InMemoryCache, createHttpLink, split } from '@apollo/client'
+import { getMainDefinition } from '@apollo/client/utilities'
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions'
+import { createUploadLink } from 'apollo-upload-client'
+import { createClient } from 'graphql-ws'
+import { getStorageLoggedInUser } from '../localStorage'
+
+
+const apiUri = 'http://localhost:8001/api'
+const wsUrl = 'http://localhost:8001/api'
+
+const httpLink = createHttpLink({
+    uri: apiUri,
+    credentials: 'include',
+})
+
+const uploadLink = createUploadLink({
+    uri: apiUri,
+    headers: {
+        'Apollo-Require-Preflight': 'true',
+    },
+}) as unknown as ApolloLink
+
+const authLink = new ApolloLink((operation, forward) => {
+    const user = getStorageLoggedInUser()
+
+    operation.setContext({
+        headers: {
+            authorization: user?.accessToken ? `Bearer ${user.accessToken}` : '',
+        },
+    })
+
+    return forward(operation)
+})
+
+const wsLink = new GraphQLWsLink(createClient({
+    url: wsUrl,
+    connectionParams: () => ({
+        accessToken: getStorageLoggedInUser()?.accessToken,
+    }),
+}))
+
+const splitLink = split(
+    operation => operation.getContext().hasUpload,
+    authLink.concat(uploadLink),
+    split(
+        ({ query }) => {
+            const definition = getMainDefinition(query)
+            return (
+                definition.kind === 'OperationDefinition' &&
+                definition.operation === 'subscription'
+            )
+        },
+        wsLink,
+        authLink.concat(httpLink),
+    ),
+)
+
+const cache = new InMemoryCache({})
+
+const client = new ApolloClient({
+    link: splitLink,
+    cache,
+})
+
+export default client
