36b5202de911228ec5d37d928fd55482a000dc29
davidcoderistov <davidcoderistov@gmail.com>
2023-07-10 22:10:36 +0200

    Write findMessagesByChatId query

--- src/graphql/queries/chat/index.ts
+++ src/graphql/queries/chat/index.ts
@@ -35,3 +35,23 @@ export const FIND_CHATS_FOR_USER = gql`
         }
     }
 `
+
+export const FIND_MESSAGES_BY_CHAT_ID = gql`
+    query findMessagesByChatId($chatId: String!, $limit: Int!, $offset: Int!) {
+        findMessagesByChatId(chatId: $chatId, offset: $offset, limit: $limit) {
+            count
+            data {
+                _id
+                creator {
+                    _id
+                    username
+                    photoUrl
+                }
+                text
+                photoUrl
+                videoUrl
+                createdAt
+            }
+        }
+    }
+`
--- src/graphql/types/queries/chat/index.ts
+++ src/graphql/types/queries/chat/index.ts
@@ -1,9 +1,16 @@
-import { ChatWithLatestMessage } from '../../models'
+import { ChatWithLatestMessage, Message } from '../../models'
 
 
 export interface FindChatsForUserQueryType {
     findChatsForUser: {
         count: number
         data: ChatWithLatestMessage[]
+    }
+}
+
+export interface FindMessagesByChatIdQueryType {
+    findMessagesByChatId: {
+        count: number
+        data: Message[]
     }
 }
