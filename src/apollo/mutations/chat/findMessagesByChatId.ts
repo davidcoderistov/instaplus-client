55f2c6429169fb875bb809efcf87da3746f0389d
davidcoderistov <davidcoderistov@gmail.com>
2023-07-25 12:01:09 +0200

    Write reactToMessage apollo mutation

--- src/apollo/mutations/chat/findMessagesByChatId.ts
+++ src/apollo/mutations/chat/findMessagesByChatId.ts
@@ -1,5 +1,6 @@
 import { FindMessagesByChatIdQueryType } from '../../../graphql/types/queries/chat'
 import { Message } from '../../../graphql/types/models'
+import { v4 as uuidv4 } from 'uuid'
 
 
 interface AddMessageOptions {
@@ -52,9 +53,64 @@ export function updateMessage(options: UpdateMessageOptions): UpdateMessageRetur
     }
 }
 
+interface ReactToMessageOptions {
+    queryData: FindMessagesByChatIdQueryType
+    variables: {
+        messageId: string,
+        reaction: string,
+        creator: {
+            _id: string
+            firstName: string
+            lastName: string
+            username: string
+            photoUrl: string | null
+        }
+    }
+}
+
+interface ReactToMessageReturnValue {
+    queryResult: FindMessagesByChatIdQueryType
+}
+
+export function reactToMessage(options: ReactToMessageOptions): ReactToMessageReturnValue {
+    return {
+        queryResult: {
+            findMessagesByChatId: {
+                ...options.queryData.findMessagesByChatId,
+                data: options.queryData.findMessagesByChatId.data.map(message => {
+                    if (message._id === options.variables.messageId) {
+                        const reactions = Array.isArray(message.reactions) ? message.reactions : []
+                        let reactionFound = false
+                        const newReactions = reactions.map(reaction => {
+                            if (reaction.creator._id === options.variables.creator._id) {
+                                reactionFound = true
+                                return {
+                                    ...reaction,
+                                    reaction: options.variables.reaction,
+                                }
+                            }
+                            return reaction
+                        })
+                        return {
+                            ...message,
+                            reactions: reactionFound ? newReactions : [...reactions, {
+                                _id: uuidv4(),
+                                reaction: options.variables.reaction,
+                                creator: options.variables.creator,
+                            }],
+                        }
+                    }
+                    return message
+                }),
+            },
+        },
+    }
+}
+
 const mutations = {
     addMessage,
     updateMessage,
+    reactToMessage,
 }
 
 export default mutations
