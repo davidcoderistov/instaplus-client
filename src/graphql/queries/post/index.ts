d75e7e31d1a98cae3cd5f1e3acca075b4ec2bedf
davidcoderistov <davidcoderistov@gmail.com>
2023-08-07 14:52:41 +0200

    Write findFollowedUsersPosts graphql query

--- src/graphql/queries/post/index.ts
+++ src/graphql/queries/post/index.ts
@@ -10,3 +10,36 @@ export const FIND_HASHTAGS_BY_SEARCH_QUERY = gql`
         }
     }
 `
+
+export const FIND_FOLLOWED_USERS_POSTS = gql`
+    query findFollowedUsersPosts($cursor: Cursor, $limit: Int!) {
+        findFollowedUsersPosts(cursor: $cursor, limit: $limit) {
+            data {
+                _id
+                post {
+                    _id
+                    caption
+                    location
+                    photoUrls
+                    createdAt
+                }
+                liked
+                saved
+                commentsCount
+                likesCount
+                latestLikeUser {
+                    _id
+                    username
+                }
+                latestThreeFollowedLikeUsers {
+                    _id
+                    photoUrl
+                }
+            }
+            nextCursor {
+                _id
+                createdAt
+            }
+        }
+    }
+`
--- src/graphql/types/queries/post/index.ts
+++ src/graphql/types/queries/post/index.ts
@@ -1,6 +1,14 @@
-import { Hashtag } from '../../models'
+import { Hashtag, PostDetails } from '../../models'
+import { NextCursor } from '../../../../types'
 
 
 export interface FindHashtagsBySearchQueryQueryType {
     findHashtagsBySearchQuery: Hashtag[]
+}
+
+export interface FindFollowedUsersPostsQueryType {
+    findFollowedUsersPosts: {
+        nextCursor: NextCursor | null
+        data: PostDetails[]
+    }
 }
