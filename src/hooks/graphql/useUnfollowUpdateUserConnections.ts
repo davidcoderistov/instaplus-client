cafc8ad62a774fc70013a9a6d473509c8426e2a6
davidcoderistov <davidcoderistov@gmail.com>
2023-08-20 13:16:44 +0200

    Init useUnfollowUpdateUserConnections graphql hook

---
+++ src/hooks/graphql/useUnfollowUpdateUserConnections.ts
@@ -0,0 +1,9 @@
+import { FollowableUser } from '../../graphql/types/models'
+
+
+export function useUnfollowUpdateUserConnections() {
+
+    return (followableUser: FollowableUser) => {
+        // TODO: Implement method
+    }
+}
