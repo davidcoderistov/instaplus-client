99a89c9e9cef13dbc9f2abf8a5629ea9e1292257
davidcoderistov <davidcoderistov@gmail.com>
2023-08-20 13:08:49 +0200

    Init useFollowUpdateUserConnections graphql hook

---
+++ src/hooks/graphql/useFollowUpdateUserConnections.ts
@@ -0,0 +1,9 @@
+import { FollowableUser } from '../../graphql/types/models'
+
+
+export function useFollowUpdateUserConnections() {
+
+    return (followableUser: FollowableUser) => {
+        // TODO: Implement method
+    }
+}
