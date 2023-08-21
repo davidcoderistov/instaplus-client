772e3b3e394c1dbf2854a360787f34fb32b8708b
davidcoderistov <davidcoderistov@gmail.com>
2023-08-21 19:02:11 +0200

    Implement useUnsavePost hook

---
+++ src/hooks/graphql/useUnsavePost.ts
@@ -0,0 +1,42 @@
+import { useMemo } from 'react'
+import { useMutation, useApolloClient, gql } from '@apollo/client'
+import { UNSAVE_POST } from '../../graphql/mutations/post'
+import { PostDetails } from '../../graphql/types/models'
+import _debounce from 'lodash/debounce'
+
+
+export function useUnsavePost() {
+
+    const client = useApolloClient()
+
+    const [unsavePost] = useMutation(UNSAVE_POST)
+
+    const _unsavePost = useMemo(() => _debounce((postId: string) => {
+        unsavePost({
+            variables: {
+                postId,
+            },
+        }).then(() => {
+            // TODO: Remove post from user favorites
+        }).catch(console.log)
+    }, 500, { trailing: true }), [])
+
+    return (postId: string) => {
+        client.cache.updateFragment({
+            id: `PostDetails:${postId}`,
+            fragment: gql`
+                fragment PostDetails on PostDetails {
+                    saved
+                }
+            `,
+        }, (postDetails: Pick<PostDetails, 'saved'> | null) => {
+            if (postDetails) {
+                return {
+                    ...postDetails,
+                    saved: false,
+                }
+            }
+        })
+        _unsavePost(postId)
+    }
+}
