fb4f8b8b40d07ae0878e49cd1c3bab74f1713f32
davidcoderistov <davidcoderistov@gmail.com>
2023-08-21 19:01:50 +0200

    Implement useSavePost hook

---
+++ src/hooks/graphql/useSavePost.ts
@@ -0,0 +1,42 @@
+import { useMemo } from 'react'
+import { useMutation, useApolloClient, gql } from '@apollo/client'
+import { SAVE_POST } from '../../graphql/mutations/post'
+import { PostDetails } from '../../graphql/types/models'
+import _debounce from 'lodash/debounce'
+
+
+export function useSavePost() {
+
+    const client = useApolloClient()
+
+    const [savePost] = useMutation(SAVE_POST)
+
+    const _savePost = useMemo(() => _debounce((postId: string) => {
+        savePost({
+            variables: {
+                postId,
+            },
+        }).then(() => {
+            // TODO: Add post to user favorites
+        }).catch(console.log)
+    }, 500, { trailing: true }), [])
+
+    return (postId: string) => {
+        client.cache.updateFragment({
+            id: `PostDetails:${postId}`,
+            fragment: gql`
+                fragment PostDetails on PostDetails {
+                    saved
+                }
+            `,
+        }, (postDetails: Pick<PostDetails, 'saved'> | null) => {
+            if (postDetails) {
+                return {
+                    ...postDetails,
+                    saved: true,
+                }
+            }
+        })
+        _savePost(postId)
+    }
+}
