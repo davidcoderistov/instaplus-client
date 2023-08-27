8609a7de6f5d66fcd4bf220c83e146f20e273d20
davidcoderistov <davidcoderistov@gmail.com>
2023-08-27 12:46:46 +0200

    Create tabs in user details component

--- src/components/UserDetails/index.tsx
+++ src/components/UserDetails/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useAuthUser } from '../../hooks/misc'
 import { useLocation, useParams } from 'react-router-dom'
 import { useQuery } from '@apollo/client'
@@ -8,6 +8,114 @@ import Box from '@mui/material/Box'
 import ProfileDescription from '../../lib/src/components/ProfileDescription'
 import FollowersModal from '../FollowersModal'
 import FollowingModal from '../FollowingModal'
+
+
+const Tab = (props: { name: string, active: boolean, icon: React.ReactNode, onClick(): void }) => {
+
+    return (
+        <Box
+            component='div'
+            display='flex'
+            alignItems='center'
+            color={props.active ? '#F5F5F5' : '#A8A8A8'}
+            marginRight='60px'
+            borderTop={props.active ? '1px solid #F5F5F5' : '0'}
+            marginTop='-1px!important'
+            flexDirection='row'
+            height='52px'
+            justifyContent='center'
+            textTransform='uppercase'
+            padding='0'
+            bgcolor='transparent'
+            boxSizing='border-box'
+            sx={{ cursor: 'pointer', touchAction: 'manipulation' }}
+            onClick={props.onClick}
+        >
+            <Box
+                component='div'
+                display='flex'
+                bgcolor='transparent'
+                boxSizing='border-box'
+                alignItems='center'
+                flexShrink='0'
+                flexDirection='row'
+                alignSelf='auto'
+                justifyContent='flex-start'
+                position='relative'
+                flexGrow='0'
+                sx={{
+                    overflowY: 'visible',
+                    overflowX: 'visible',
+                }}
+            >
+                <svg
+                    aria-label=''
+                    style={{ display: 'block', position: 'relative' }}
+                    color={props.active ? 'rgb(245, 245, 245)' : 'rgb(168, 168, 168)'}
+                    fill={props.active ? 'rgb(245, 245, 245)' : 'rgb(168, 168, 168)'}
+                    viewBox='0 0 24 24'
+                    height='12'
+                    width='12'
+                    role='img'
+                >
+                    {props.icon}
+                </svg>
+                <Box
+                    component='span'
+                    padding='0'
+                    fontSize='100%'
+                    marginLeft='6px'
+                    sx={{ verticalAlign: 'baseline' }}
+                >
+                    {props.name}
+                </Box>
+            </Box>
+        </Box>
+    )
+}
+
+const PostsTab = (props: { active: boolean, onClick(): void }) => {
+
+    return (
+        <Tab
+            name='Posts'
+            active={props.active}
+            onClick={props.onClick}
+            icon={<>
+                <rect fill='none' height='18' stroke='currentColor' strokeLinecap='round'
+                      strokeLinejoin='round' strokeWidth='2' width='18' x='3' y='3' />
+                <line fill='none' stroke='currentColor' strokeLinecap='round'
+                      strokeLinejoin='round' strokeWidth='2' x1='9.015' x2='9.015' y1='3'
+                      y2='21' />
+                <line fill='none' stroke='currentColor' strokeLinecap='round'
+                      strokeLinejoin='round' strokeWidth='2' x1='14.985' x2='14.985' y1='3'
+                      y2='21' />
+                <line fill='none' stroke='currentColor' strokeLinecap='round'
+                      strokeLinejoin='round' strokeWidth='2' x1='21' x2='3' y1='9.015'
+                      y2='9.015' />
+                <line fill='none' stroke='currentColor' strokeLinecap='round'
+                      strokeLinejoin='round' strokeWidth='2' x1='21' x2='3' y1='14.985'
+                      y2='14.985' />
+            </>} />
+    )
+}
+
+const SavedTab = (props: { active: boolean, onClick(): void }) => {
+
+    return (
+        <Tab
+            name='Saved'
+            active={props.active}
+            onClick={props.onClick}
+            icon={<polygon
+                fill='none'
+                points='20 21 12 13.44 4 21 4 3 20 3 20 21'
+                stroke='currentColor'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth='2' />} />
+    )
+}
 
 
 export default function UserDetails() {
@@ -46,9 +154,20 @@ export default function UserDetails() {
         setViewFollowingUserId(null)
     }
 
+    const [isPostsTabActive, setIsPostsTabActive] = useState(true)
+
+    const handleClickPostsTab = () => {
+        setIsPostsTabActive(true)
+    }
+
+    const handleClickSavedTab = () => {
+        setIsPostsTabActive(false)
+    }
+
     useEffect(() => {
         setViewFollowersUserId(null)
         setViewFollowingUserId(null)
+        setIsPostsTabActive(true)
     }, [location])
 
     return (
@@ -115,6 +234,48 @@ export default function UserDetails() {
                                 onViewFollowers={handleViewFollowers}
                                 onViewFollowing={handleViewFollowing}
                             />
+                        </Box>
+                        <Box
+                            component='div'
+                            paddingTop='4vh'
+                            marginRight='auto'
+                            marginTop='0'
+                            paddingRight='20px'
+                            paddingLeft='20px'
+                            marginBottom='16px'
+                            flexGrow='1'
+                            marginLeft='auto'
+                            maxWidth='1035px'
+                            width='calc(100% - 40px)'
+                            paddingBottom='0'
+                            display='block'
+                        >
+                            <Box
+                                component='div'
+                                fontWeight='600'
+                                justifyContent='center'
+                                padding='0'
+                                marginTop='44px'
+                                border='0'
+                                boxSizing='border-box'
+                                display='flex'
+                                alignItems='center'
+                                textAlign='center'
+                                color='#8E8E8E'
+                                letterSpacing='1px'
+                                flexDirection='row'
+                                fontSize='0.75rem'
+                                position='relative'
+                                borderTop='1px solid #262626'
+                                sx={{ verticalAlign: 'baseline' }}
+                            >
+                                <PostsTab
+                                    active={isPostsTabActive}
+                                    onClick={handleClickPostsTab} />
+                                <SavedTab
+                                    active={!isPostsTabActive}
+                                    onClick={handleClickSavedTab} />
+                            </Box>
                         </Box>
                     </Box>
                 ) : (
