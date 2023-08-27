101bdd37181ed0e3f33a34c2a0a45fccb733bfe4
davidcoderistov <davidcoderistov@gmail.com>
2023-08-27 14:33:15 +0200

    Refactor useUserDetailsNavigation

--- src/hooks/misc/useUserDetailsNavigation.ts
+++ src/hooks/misc/useUserDetailsNavigation.ts
@@ -1,11 +1,18 @@
 import { useNavigate } from 'react-router-dom'
+import { useAuthUser } from './useAuthUser'
 
 
 export function useUserDetailsNavigation() {
 
     const navigate = useNavigate()
 
+    const [authUser] = useAuthUser()
+
     return (userId: any) => {
-        navigate(`/user/${userId}`)
+        if (authUser._id === userId) {
+            navigate('/profile')
+        } else {
+            navigate(`/user/${userId}`)
+        }
     }
 }
