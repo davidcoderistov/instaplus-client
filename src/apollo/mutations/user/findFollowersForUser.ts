a21dc48deccca0c2cb2e0612ab09e55be8e51681
davidcoderistov <davidcoderistov@gmail.com>
2023-08-29 14:57:18 +0200

    Add removeFollowerUser apollo mutation

--- src/apollo/mutations/user/findFollowersForUser.ts
+++ src/apollo/mutations/user/findFollowersForUser.ts
@@ -26,8 +26,39 @@ export function addFollowerUser(options: AddFollowerUserOptions): AddFollowerUse
     }
 }
 
+interface RemoveFollowerUserOptions {
+    queryData: FindFollowersForUserQueryType
+    variables: {
+        userId: string
+    }
+}
+
+interface RemoveFollowerUserReturnValue {
+    queryResult: FindFollowersForUserQueryType
+}
+
+export function removeFollowerUser(options: RemoveFollowerUserOptions): RemoveFollowerUserReturnValue {
+
+    const data = Array.from(options.queryData.findFollowersForUser.data)
+    const findFollowerUserId = data.findIndex(followerUser => followerUser.user._id === options.variables.userId)
+
+    if (findFollowerUserId >= 0) {
+        data.splice(findFollowerUserId, 1)
+    }
+    return {
+        queryResult: {
+            ...options.queryData,
+            findFollowersForUser: {
+                ...options.queryData.findFollowersForUser,
+                data,
+            },
+        },
+    }
+}
+
 const mutations = {
     addFollowerUser,
+    removeFollowerUser,
 }
 
 export default mutations
