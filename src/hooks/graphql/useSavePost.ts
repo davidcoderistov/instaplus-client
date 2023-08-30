4b9cc44a5efa121842c5da6adcad28bc98e63743
davidcoderistov <davidcoderistov@gmail.com>
2023-08-30 12:29:24 +0200

    Implement add and remove saved posts

--- src/hooks/graphql/useSavePost.ts
+++ src/hooks/graphql/useSavePost.ts
@@ -1,6 +1,10 @@
 import { useMemo } from 'react'
 import { useMutation, useApolloClient, gql } from '@apollo/client'
+import { addSavedPost } from '../../apollo/mutations/post/findSavedPostsForUser'
 import { SAVE_POST } from '../../graphql/mutations/post'
+import { SavePostMutationType } from '../../graphql/types/mutations/post'
+import { FIND_SAVED_POSTS_FOR_USER } from '../../graphql/queries/post'
+import { FindSavedPostsForUserQueryType } from '../../graphql/types/queries/post'
 import { PostDetails } from '../../graphql/types/models'
 import _debounce from 'lodash/debounce'
 
@@ -9,15 +13,26 @@ export function useSavePost() {
 
     const client = useApolloClient()
 
-    const [savePost] = useMutation(SAVE_POST)
+    const [savePost] = useMutation<SavePostMutationType>(SAVE_POST)
 
     const _savePost = useMemo(() => _debounce((postId: string) => {
         savePost({
             variables: {
                 postId,
             },
-        }).then(() => {
-            // TODO: Add post to user favorites
+        }).then(({ data }) => {
+            if (data) {
+                client.cache.updateQuery({
+                    query: FIND_SAVED_POSTS_FOR_USER,
+                }, (findSavedPostsForUser: FindSavedPostsForUserQueryType | null) => {
+                    if (findSavedPostsForUser) {
+                        return addSavedPost({
+                            queryData: findSavedPostsForUser,
+                            variables: { post: data.savePost },
+                        }).queryResult
+                    }
+                })
+            }
         }).catch(() => {
         })
     }, 500, { trailing: true }), [])
--- src/hooks/graphql/useUnsavePost.ts
+++ src/hooks/graphql/useUnsavePost.ts
@@ -1,6 +1,10 @@
 import { useMemo } from 'react'
 import { useMutation, useApolloClient, gql } from '@apollo/client'
+import { removeSavedPost } from '../../apollo/mutations/post/findSavedPostsForUser'
 import { UNSAVE_POST } from '../../graphql/mutations/post'
+import { UnsavePostMutationType } from '../../graphql/types/mutations/post'
+import { FIND_SAVED_POSTS_FOR_USER } from '../../graphql/queries/post'
+import { FindSavedPostsForUserQueryType } from '../../graphql/types/queries/post'
 import { PostDetails } from '../../graphql/types/models'
 import _debounce from 'lodash/debounce'
 
@@ -9,15 +13,26 @@ export function useUnsavePost() {
 
     const client = useApolloClient()
 
-    const [unsavePost] = useMutation(UNSAVE_POST)
+    const [unsavePost] = useMutation<UnsavePostMutationType>(UNSAVE_POST)
 
     const _unsavePost = useMemo(() => _debounce((postId: string) => {
         unsavePost({
             variables: {
                 postId,
             },
-        }).then(() => {
-            // TODO: Remove post from user favorites
+        }).then(({ data }) => {
+            if (data) {
+                client.cache.updateQuery({
+                    query: FIND_SAVED_POSTS_FOR_USER,
+                }, (findSavedPostsForUser: FindSavedPostsForUserQueryType | null) => {
+                    if (findSavedPostsForUser) {
+                        return removeSavedPost({
+                            queryData: findSavedPostsForUser,
+                            variables: { postId: data.unsavePost._id },
+                        }).queryResult
+                    }
+                })
+            }
         }).catch(() => {
         })
     }, 500, { trailing: true }), [])
