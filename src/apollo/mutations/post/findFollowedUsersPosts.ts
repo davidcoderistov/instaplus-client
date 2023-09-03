69e0bfc29d2ecb6bb6cac6df1a9b844b866ad5ea
davidcoderistov <davidcoderistov@gmail.com>
2023-09-03 14:03:53 +0200

    Add incrementCommentsCount post apollo mutation

--- src/apollo/mutations/post/findFollowedUsersPosts.ts
+++ src/apollo/mutations/post/findFollowedUsersPosts.ts
@@ -26,8 +26,41 @@ export function addPost(options: AddPostOptions): AddPostReturnValue {
     }
 }
 
+interface IncrementCommentsCountOptions {
+    queryData: FindFollowedUsersPostsQueryType
+    variables: {
+        postId: string
+    }
+}
+
+interface IncrementCommentsCountReturnValue {
+    queryResult: FindFollowedUsersPostsQueryType
+}
+
+export function incrementCommentsCount(options: IncrementCommentsCountOptions): IncrementCommentsCountReturnValue {
+
+    return {
+        queryResult: {
+            ...options.queryData,
+            findFollowedUsersPosts: {
+                ...options.queryData.findFollowedUsersPosts,
+                data: options.queryData.findFollowedUsersPosts.data.map(post => {
+                    if (post._id === options.variables.postId) {
+                        return {
+                            ...post,
+                            commentsCount: post.commentsCount + 1,
+                        }
+                    }
+                    return post
+                }),
+            },
+        },
+    }
+}
+
 const mutations = {
     addPost,
+    incrementCommentsCount,
 }
 
 export default mutations
