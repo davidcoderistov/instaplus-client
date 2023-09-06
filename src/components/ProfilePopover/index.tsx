9c7d7d4a81a1e10edb551f9ddbc4f8681247cf21
davidcoderistov <davidcoderistov@gmail.com>
2023-09-06 11:59:55 +0200

    Create ProfilePopover component

---
+++ src/components/ProfilePopover/index.tsx
@@ -0,0 +1,79 @@
+import { useQuery } from '@apollo/client'
+import { FIND_USER_DETAILS } from '../../graphql/queries/user'
+import { FindUserDetailsQueryType } from '../../graphql/types/queries/user'
+import { FIND_LATEST_POSTS_FOR_USER } from '../../graphql/queries/post'
+import { FindLatestPostsForUserQueryType } from '../../graphql/types/queries/post'
+import { PopupState, bindPopover } from 'material-ui-popup-state/hooks'
+import HoverPopover from 'material-ui-popup-state/HoverPopover'
+import ProfileCard from '../../lib/src/components/ProfileCard'
+
+
+interface Props {
+    popupState: PopupState
+    userId: string | null
+}
+
+export default function ProfilePopover(props: Props) {
+
+    const userDetails = useQuery<FindUserDetailsQueryType>(FIND_USER_DETAILS, {
+        variables: { userId: props.userId },
+        skip: Boolean(!props.userId),
+    })
+
+    const latestPosts = useQuery<FindLatestPostsForUserQueryType>(FIND_LATEST_POSTS_FOR_USER, {
+        variables: {
+            userId: props.userId,
+            limit: 3,
+        },
+        skip: Boolean(!props.userId),
+    })
+
+    return (
+        <HoverPopover
+            {...bindPopover(props.popupState)}
+            anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+            }}
+            transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+            }}
+            sx={{
+                '& .MuiPopover-paper': {
+                    backgroundColor: '#000000',
+                },
+                zIndex: 999999,
+            }}
+        >
+            {userDetails.loading || latestPosts.loading ? (
+                <ProfileCard loading />
+            ) : !userDetails.error && userDetails.data && !latestPosts.error && latestPosts.data ? (
+                <ProfileCard
+                    user={{
+                        id: userDetails.data.findUserDetails.followableUser.user._id,
+                        username: userDetails.data.findUserDetails.followableUser.user.username,
+                        firstName: userDetails.data.findUserDetails.followableUser.user.firstName,
+                        lastName: userDetails.data.findUserDetails.followableUser.user.lastName,
+                        photoUrl: userDetails.data.findUserDetails.followableUser.user.photoUrl,
+                        following: userDetails.data.findUserDetails.followableUser.following,
+                        followingLoading: userDetails.data.findUserDetails.followableUser.followingLoading,
+                    }}
+                    postsCount={userDetails.data.findUserDetails.postsCount}
+                    followersCount={userDetails.data.findUserDetails.followersCount}
+                    followingCount={userDetails.data.findUserDetails.followingCount}
+                    posts={latestPosts.data.findLatestPostsForUser.map(post => ({
+                        id: post._id,
+                        photoUrl: post.photoUrls[0],
+                        multiple: false,
+                    }))}
+                    onClickUser={console.log}
+                    onMessageUser={console.log}
+                    onFollowUser={console.log}
+                    onUnfollowUser={console.log}
+                    onClickPost={console.log}
+                />
+            ) : null}
+        </HoverPopover>
+    )
+}
